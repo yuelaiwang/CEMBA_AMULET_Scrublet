d121bb313841987b1cd179e4ac01b69eabb4cb94
yuelaiwang <eliwang818@gmail.com>
2022-08-31 07:53:10 -0700

    update 8/31

--- script/simulate_cemba_doublets.py
+++ script/simulate_cemba_doublets.py
@@ -5,18 +5,18 @@ import random
 import argparse
 parser = argparse.ArgumentParser(description = "Generate bam files each containing singlets and simulated doublets merged from singlets. For example, if there are 8800 singlets, and the proportion of doublets is set to be 0.1, then 1600 random singlets would be selected to generate 800 doublets, and the final bam file would contain 8000 cells in total with 7200 singlets and 800 doublets.")
 parser.add_argument('bam', type = str, help='the path to the parent bam file')
-parser.add_argument('annotation', type = str, help = "the path to the annotation file")
+parser.add_argument('annotation', type = str, help = "the path to an tsv annotation file, here is metatable.tsv")
 parser.add_argument("classifier", type = str, help = "the column name in the annotation file used to classify cell type for doublet annotation as homo/hetero")
-parser.add_argument('--region', type=str, dest = "region", default = "None", help = 'the region name, e.g.CEMBA171213_4B')
-parser.add_argument("singlets", type=str, help='the path to the \"SingletBarcodes_01.txt\" file')
-parser.add_argument("summary", type=str, help='the path to the \"OverlapSummary.txt\" file given by running AMULET on the original bam file of this region/replication')
+parser.add_argument("singlets", type = str, help='the path to the \"SingletBarcodes_01.txt\" file')
+parser.add_argument("summary", type = str, help = 'the path to the \"OverlapSummary.txt\" file given by running AMULET on the original bam file of this region/replication. This is used to record the number of uniquely aligned fragments for each singlet')
 parser.add_argument("--fclower", type = int, dest = "low", default = 0, help = "an integer number that the script would only pick cells with a fragment counts above")
 parser.add_argument("--fchigher", type = int, dest = "high", default = 100000000, help = "an integer number that the script would only pick cells with a fragment counts below")
 parser.add_argument("--poolsize", type = int, dest = "size", default = 100000, help = "the number of singlets to keep in the pool (when downsampling, need to keep the pool size the same)")
 parser.add_argument('--proportion', type=float, dest="proportion", default = 0.1, help='the proportion of doublets in a simulated dataset')
 parser.add_argument('--homoratio', type = str, dest = "homoratio", default = "False", help = "whether to generate constant homotypic doublet ratio datasets, TRUE means yes and FALSE means no")
-parser.add_argument('location', type=str, help='the location to store generated bam files and corresponding text files of simulated doublet barcodes. The text file will be a 6-column tsv file whose columns are doublet_barcode, singlet1_barcode, singlet2_barcode, singlet1_majortype, singlet2_majortype, and doublet_type.')
+parser.add_argument('location', type=str, help='the location to store generated bam files and corresponding ground truth file of simulated doublet barcodes. The groung truth file will be a 6-column tsv file whose columns are doublet_barcode, singlet1_barcode, singlet2_barcode, singlet1_majortype, singlet2_majortype, and doublet_type.')
 parser.add_argument("--cellID", type = str, default = "Barcode", help = "the column in the CEMBA 1.0 metatable that is used here")
+parser.add_argument('--region', type=str, dest = "region", default = "None", help = 'the region name, e.g.CEMBA171213_4B. This is totally optional, specifying it would speed up the execution of the script a bit.')
 
 args = parser.parse_args()
 cellID = args.cellID
@@ -100,36 +100,23 @@ def annotateCellType(annotationFile, classifier, region) -> dict:
     cellTypeAnnotation = dict() # keys are barcodes, and values are major type
     annotationTable = pd.read_csv(annotationFile, sep = '\t')
     if region != "None":
-        regionalTable = annotationTable[annotationTable["Sample"] == region]
+        regionalTable = annotationTable[annotationTable["Sample"] == region] # the name "Sample" corresponds to the column name in the metatable
     else:
         regionalTable = annotationTable
     for i in regionalTable.index:
-        barcode = regionalTable.loc[i, cellID]
-        # because I added a character to the barcode from different region, I have to add again at this place.
-        '''
-        rep_region = regionalTable.loc[i, "Sample"]
-        if rep_region == "CEMBA180409_2C":
-            barcode = 'A' + barcode
-        elif rep_region == "CEMBA171206_3C" or rep_region == "CEMBA171207_3C":
-            barcode = 'C' + barcode
-        elif rep_region == "CEMBA171213_4B" or rep_region == "CEMBA180104_4B":
-            barcode = 'G' + barcode
-        elif rep_region == "CEMBA180618_5D" or rep_region == "CEMBA180612_5D":
-            barcode = 'T' + barcode
-        '''
+        barcode = regionalTable.loc[i, cellID] # cellID is customizable
         cellType = regionalTable.loc[i, classifier]
         cellTypeAnnotation[barcode] = cellType
     return cellTypeAnnotation
 
-# The number of artificial barcodes to be generated is N(singlets) * proportion / (1 + proportion), and the number of keys should be double the amount of that since we are only generating doublets.
-# generate a 6-column pandas dataframe, where the columns are, in order, artificial doublet barcode, singlet1 barcode, singlet2 barcode, singlet1 major type, singlet2 major type, and doublet type. 
+# This function generates artificial barcodes. It stores the information into a 6-column pandas dataframe, where the columns are, in order, artificial doublet barcode, singlet1 barcode, singlet2 barcode, singlet1 major type, singlet2 major type, and doublet type. The number of artificial barcodes to be generated is N(singlets) * proportion / (1 + proportion). The dataframe would be stored as a tsv file called ground.truth.tsv after running the script. 
 def GenerateBarcodes(singlets : list, cellType : dict, proportion) -> dict:
     multiDict = {"doubletBarcode" : [], "singlet1Barcode" : [], "singlet2Barcode": [], "singlet1MajorType" : [], "singlet2MajorType" : [], "doubletType" : []}
     artificialBarcodes = pd.DataFrame(multiDict)
     nucleotides = ['A', 'C', 'G', 'T']
     lengthOfBarcodes = len(singlets[0])
     numberOfArtificialBarcodes = int(proportion * len(singlets) / (1 + proportion))
-    # generate a list of random indices in singlets. These singlets will be replaced
+    # generate a list of random indices in singlets. These singlets will be replaced by doublets
     singletsToReplace = random.sample(range(0, len(singlets)), 2 * numberOfArtificialBarcodes)
     for j in range(numberOfArtificialBarcodes):
         artificialBarcode = ""
@@ -146,12 +133,10 @@ def GenerateBarcodes(singlets : list, cellType : dict, proportion) -> dict:
         if singlet1type == singlet2type:
             doubletType = "homotypic"
         artificialBarcodes.loc[len(artificialBarcodes.index)] = [artificialBarcode, singlet1, singlet2, singlet1type, singlet2type, doubletType]
-        # artificialBarcodes[singlets[singletsToReplace[j * 2]]] = artificialBarcode
-        # artificialBarcodes[singlets[singletsToReplace[j * 2 + 1]]] = artificialBarcode
 
     return artificialBarcodes
 
-# generate a list of barcodes in this region after replacing some barcodes with artificial barcodes. This function is used for regenerating the singlecell.csv file
+# This function generates a list of barcodes in this region after replacing some barcodes with artificial barcodes. This function is used for regenerating the singlecell.csv file for running AMULET.
 def WholeSetOfBarcodes(artificialBarcodes, singlets : list) -> list:
     wholeset = list()
     singlet1Barcodes = list(artificialBarcodes["singlet1Barcode"])
@@ -169,20 +154,20 @@ def WholeSetOfBarcodes(artificialBarcodes, singlets : list) -> list:
     uniqueSet = list(set(wholeset))
     return uniqueSet
 
-# generate a bam file that contains artificial barcodes. These barcodes replace original barcodes.
+# generate a bam file that contains artificial doublet barcodes. These barcodes replace singlet barcodes that are picked to form the artificial doublets.
 def GenerateSimulatedDataset(samfile, simulation, singlets, artificialBarcodes):
     singlet1Barcodes = list(artificialBarcodes["singlet1Barcode"])
     singlet2Barcodes = list(artificialBarcodes["singlet2Barcode"])
-    for record in samfile.fetch(until_eof = True):
+    for record in samfile.fetch(until_eof = True): # don't have to make index file for the bam file
         barcode = record.query_name.split(':')[0]
         newRecord = record
-        if barcode in singlet1Barcodes:
+        if barcode in singlet1Barcodes: # if the barcode is a singlet to be merged into a doublet
             index = singlet1Barcodes.index(barcode)
-            artificialBarcode = artificialBarcodes.loc[index, "doubletBarcode"]
-            newRecord.set_tag("CB", artificialBarcode)
+            artificialBarcode = artificialBarcodes.loc[index, "doubletBarcode"] # obtain the artificial doublet barcode
+            newRecord.set_tag("CB", artificialBarcode) # set CB attribute to be the artificial barcode
             queryName = newRecord.query_name
             queryNameSuffix = queryName[len(barcode):]
-            newRecord.query_name = artificialBarcode + queryNameSuffix
+            newRecord.query_name = artificialBarcode + queryNameSuffix # fix the query name, the first field of a bam file, to have proper input for Scrublet
             simulation.write(newRecord)
         elif barcode in singlet2Barcodes:
             index = singlet2Barcodes.index(barcode)
